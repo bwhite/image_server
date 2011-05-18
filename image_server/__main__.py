64d6bcfc63dabdc6b84b5afc72459cae678e09a2
Brandyn A. White <bwhite@dappervision.com>
2011-05-18 00:38:56 -0400

    Initial commit Project (tp) Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ image_server/__main__.py
@@ -0,0 +1,44 @@
+import web
+import os
+import web
+import cStringIO as StringIO
+import Image
+
+urls = ('/', 'main',
+        '/(.*)\.(png|jpg|gif|ico)', 'images' #this is where the image folder is located....
+        )
+
+class main(object):
+    def GET(self):
+        image_extensions = set(['jpg', 'png', 'jpeg', 'gif', 'ico'])
+        extension = lambda x: x.split('.')[-1] if '.' in x else ''
+        local_images = [(x, x) for x in os.listdir('.') if extension(x) in image_extensions]
+        web.header("Content-Type", 'text/html')
+        render = web.template.frender(os.path.dirname(__file__) + '/image_serve_template.html')
+        return render(local_images)
+
+class images:
+    def GET(self, name, extension):
+        cType = {
+            "png":"images/png",
+            "jpg":"image/jpeg",
+            "gif":"image/gif",
+            "ico":"image/x-icon"            }
+        file_name = '%s.%s' % (name, extension)
+        if file_name in os.listdir('.'):  # Security
+            web.header("Content-Type", cType[extension]) # Set the Header
+            img = Image.open(file_name)
+            width, height = img.size
+            width = int(width * 50 / float(height))
+            img = img.resize((width, 50))
+            fp = StringIO.StringIO()
+            img.save(fp, extension)
+            fp.seek(0)
+            return fp.read()#open(file_name, 'rb').read()#fp.read()
+        else:
+            raise web.notfound()
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='image_server',
+      version='0.0.1',
+      packages=['image_server'],
+      author='Brandyn A. White',
+      author_email='bwhite@dappervision.com',
+      license='GPL',
+      package_data={'image_server':['*.html']})

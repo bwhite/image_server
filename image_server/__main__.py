46ca8986dbba6571c7282a8f9eedeaf8332e7b23
Brandyn A. White <bwhite@dappervision.com>
2011-05-28 15:24:21 -0400

    Added thumbnail

--- image_server/__main__.py
+++ image_server/__main__.py
@@ -5,19 +5,34 @@ import cStringIO as StringIO
 import Image
 
 urls = ('/', 'main',
-        '/(.*)\.(png|jpg|gif|ico)', 'images' #this is where the image folder is located....
+        '/i/(.*)\.(png|jpg|gif|ico)', 'images', #this is where the image folder is located....
+        '/t/(.*)\.(png|jpg|gif|ico)', 'thumb_images' #this is where the image folder is located....
         )
 
 class main(object):
     def GET(self):
         image_extensions = set(['jpg', 'png', 'jpeg', 'gif', 'ico'])
         extension = lambda x: x.split('.')[-1] if '.' in x else ''
-        local_images = [(x, x) for x in sorted(os.listdir('.')) if extension(x) in image_extensions]
+        local_images = [x for x in sorted(os.listdir('.')) if extension(x) in image_extensions]
         web.header("Content-Type", 'text/html')
         render = web.template.frender(os.path.dirname(__file__) + '/image_serve_template.html')
         return render(local_images)
 
-class images:
+class images(object):
+    def GET(self, name, extension):
+        cType = {
+            "png":"images/png",
+            "jpg":"image/jpeg",
+            "gif":"image/gif",
+            "ico":"image/x-icon"            }
+        file_name = '%s.%s' % (name, extension)
+        if file_name in os.listdir('.'):  # Security
+            web.header("Content-Type", cType[extension]) # Set the Header
+            return open(file_name).read()
+        else:
+            raise web.notfound()
+
+class thumb_images(object):
     def GET(self, name, extension):
         cType = {
             "png":"images/png",
@@ -37,6 +52,7 @@ class images:
             return fp.read()
         else:
             raise web.notfound()
+
 
 app = web.application(urls, globals())
 
